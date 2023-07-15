df537d3524e3445693e053a46e31a3ef86ff4a2f
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2023-07-14 22:38:36 -0500

    Enviar emails

--- lab07/settings.py
+++ lab07/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.hushmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'larocutipa@unsa.edu.pe'
+EMAIL_HOST_PASSWORD = 'luis2a2gut.'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 30
--- relaciones/views.py
+++ relaciones/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+def index(request):
+    send_mail('Hello from PrettyPrinted',
+    'Hello there. This is an automated message.',
+    'larocutipa@unsa.edu.pe',
+    ['sasaco5121@iturchia.com'],
+    fail_silently=False)
+    return render(request, 'index.html')
